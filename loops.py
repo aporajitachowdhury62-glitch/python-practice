9651bd78fb0ab6a27a871931fb0f6c79469b82e5
Aporajita Chowdhury <aporajitachowdhury62@gmail.com>
2026-08-19 22:58:19 +0530

    added new files

---
+++ loops.py
@@ -0,0 +1,15 @@
+l=['mike',10.2,1980]
+for x in l:
+    print(x)
+
+employee ={"email_id": 1001,"name":"John Gray", "department" : "HR"}
+
+for i in employee:
+    print(i,employee[i])
+
+print(employee.items())
+
+for i in employee.items():
+    print(employee.items)
+    print(i[0],i[1])
+
---
+++ strings_in_python/slicing_in_strings.py
@@ -0,0 +1,51 @@
+s1 = "Hello World"
+
+"""print(s1)
+
+#length of the string
+print(len(s1))
+
+#Indexing
+print("First char",s1[0])
+print("last char",s1[-1])
+
+syntax of indexing - string[index]
+syntax of slicing - string[start;end;step]
+"""
+
+"""print(s1[0:10:8])
+s1_slice = s1[0:10:8]
+print(type(s1_slice))"""
+
+"""name = input("Enter Name: ")
+interests = input("Enter interest: ")
+language = input("Enter language: ")
+hour = int(input("Enter hours :"))
+
+print(name,"is interested in",interests,"studies",language,"for",hour,"hours")
+
+# using fstring
+print(f"{name} is interested in {interests} studies {language} for {hour} hours")
+
+
+sub1 = 56
+sub2 = 78
+sub3 = 67
+sub4 = 78
+
+print(f"{name} got {sub1 + sub2} marks in total")
+
+percent = (sub1+sub2+sub3+sub4)/4 *100
+print(f"{name} got {percent}%.")
+"""
+## escape sequences
+""" \n = new/next line
+\t = tab
+\\ = backslash
+\' = inserts a single quote inside a single-quoted string
+
+print(" Hi,\nHow are you")
+print("JOHN\t the great ballkeeper")
+print("yes\\no")
+print('he is very handsome,he\'s son is also goodlooking')
+"""
